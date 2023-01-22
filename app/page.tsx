1ab3ea2906bdee1a9aaeffe01845f3d97dac41e6
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 14:51:03 +0530

    imported fonts from next/font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import { Fira_Code, Oswald, Roboto } from "@next/font/google";
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  variable: "--fira-code-font",
+});
+
+const oswald = Oswald({ subsets: ["latin"], variable: "--oswald-font" });
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--roboto-font",
+});
+
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className={`${firaCode.variable} ${oswald.variable} ${roboto.variable}`}>
+    </div>
   )
 }
