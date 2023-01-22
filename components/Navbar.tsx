d854d24a342c359c33329cf14fd3e50035f00fc8
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 17:32:35 +0530

    create function to close menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,10 @@ export default function Navbar() {
     setIsMenuOpen((prev) => !prev);
   }
 
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <header
       className={`h-24 top-0 fixed z-30 flex w-screen items-center justify-between px-6 transition-all duration-200 lg:px-12`}
