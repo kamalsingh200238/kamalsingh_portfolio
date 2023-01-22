dbe9a19e49bb7683b201fc445c0752fd448b1d9a
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 15:10:29 +0530

    created state to track status of mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <header
       className={`h-24 top-0 fixed z-30 flex w-screen items-center justify-between px-6 transition-all duration-200 lg:px-12`}
