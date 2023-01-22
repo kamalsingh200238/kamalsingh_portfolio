a66e6554992dcf50e0a68fcc32e1ef2ed94c13ad
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 17:49:25 +0530

    added custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,42 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: "#ced0d4",
+          200: "#9ca1a8",
+          300: "#525b67",
+          // 400: "#212c3c",
+          400: "#112240",
+          500: "#081426",
+          600: "#071222",
+          700: "#060e1b",
+          800: "#03080f",
+          900: "#010204",
+        },
+        secondary: {
+          // 100: "",
+          // 200: "",
+          // 300: "",
+          // 400: "",
+          500: "#60FFD9",
+          // 600: "",
+          // 700: "",
+          // 800: "",
+          // 900: "",
+        },
+        tertiary: {
+          // 100: "",
+          // 200: "",
+          // 300: "",
+          // 400: "",
+          500: "#ccd6f6",
+          // 600: "",
+          // 700: "",
+          // 800: "",
+          // 900: "",
+        },
+      },
       fontFamily: {
         oswald: ["var(--oswald-font)", ...defaultTheme.fontFamily.sans],
         roboto: ["var(--roboto-font)", ...defaultTheme.fontFamily.sans],
@@ -20,4 +56,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
