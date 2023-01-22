515f93814b57ad0e8211089931d38fc532cbbbd2
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 15:04:30 +0530

    added a Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header
+      className={`h-24 top-0 fixed z-30 flex w-screen items-center justify-between px-6 transition-all duration-200 lg:px-12`}
+    ></header>
+  );
+}
