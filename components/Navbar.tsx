a9e415c681cc428180691a3d69258980d1a7f710
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-22 17:39:50 +0530

    added a hamburger svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,6 +43,48 @@ export default function Navbar() {
           </g>
         </svg>
       </div>
+      <div>
+        <button
+          className="relative z-50 h-10 w-10 md:hidden"
+          onClick={toggleMenu}
+        >
+          <svg
+            className={`${isMenuOpen ? "rotate-180" : ""
+              } z-50 h-full w-full stroke-secondary-500 transition-all duration-200`}
+            viewBox="0 0 148 139"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              className={`${isMenuOpen ? "rotate-45" : ""
+                } origin-center fill-secondary-500 transition-all duration-200`}
+              x="18"
+              y={isMenuOpen ? "62" : "27"} // for better animations
+              width="112"
+              height="10"
+              rx="5"
+            />
+            <rect
+              className={`${isMenuOpen ? "opacity-0" : "opacity-100"
+                } fill-secondary-500 transition-all duration-200`}
+              x="18"
+              y="62"
+              width="112"
+              height="10"
+              rx="5"
+            />
+            <rect
+              className={`${isMenuOpen ? "-rotate-45" : ""
+                } origin-center fill-secondary-500 transition-all duration-200`}
+              x="18"
+              y={isMenuOpen ? "62" : "97"} // for better animations
+              width="112"
+              height="10"
+              rx="5"
+            />
+          </svg>
+        </button>
+      </div>
     </header>
   );
 }
