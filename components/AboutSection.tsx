be3cb44c53912286de6a566459af9d62bfff58b4
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-23 21:57:29 +0530

    getting data from json file

---
+++ components/AboutSection.tsx
@@ -0,0 +1,29 @@
+import AboutSectionChildren from "./AboutSectionChildren";
+import { AboutData } from "@/types/AboutData";
+
+import fsPromises from "fs/promises";
+import path from "path";
+async function getData() {
+  // get the file path
+  const filePath = path.join(process.cwd(), "/lib/data/AboutData.json");
+  // use the file path to read the json
+  const fileContent = await fsPromises.readFile(filePath);
+
+  const objectData = JSON.parse(fileContent.toString());
+
+  return objectData as AboutData;
+}
+
+export default async function AboutSection() {
+  const aboutData = await getData();
+  return (
+    <section
+      id="about"
+      className="px-6 py-20 sm:px-12 md:px-28 md:py-24 lg:px-36"
+    >
+      <div className="mx-auto max-w-4xl">
+          <AboutSectionChildren aboutData={aboutData} />
+      </div>
+    </section>
+  );
+}
