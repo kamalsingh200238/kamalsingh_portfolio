5723ffc35ffc851f140b42fb964d5a35edf3c733
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-25 13:32:09 +0530

    created type for workData

---
+++ types/WorkData.ts
@@ -0,0 +1,14 @@
+export interface WorkData {
+  featured: {
+    name: string;
+    images: {
+      link: string;
+      alt: string;
+    }[];
+    description: string;
+    features: string[];
+    techUsed: string[];
+    repoLink: string;
+    linkLink: string;
+  }[];
+}
