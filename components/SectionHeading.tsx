e753ce6904e70ea1e06587a311bdee7928eed820
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-25 18:01:08 +0530

    changed font size

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -10,7 +10,7 @@ export default function SectionHeading({ headingNumber, headingText }: Props) {
         <span className="mr-2 font-fira text-secondary-500 sm:text-lg lg:text-xl">
           {headingNumber}.
         </span>
-        <h2 className="inline-block font-oswald text-xl font-bold capitalize text-tertiary-500 sm:text-2xl lg:text-3xl">
+        <h2 className="inline-block font-oswald text-2xl font-bold capitalize text-tertiary-500 lg:text-3xl">
           {headingText}
         </h2>
       </div>
