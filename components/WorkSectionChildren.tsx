c15692dc99c0273811cd653b42e155c3cfd65b25
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-25 18:09:13 +0530

    changed tracking and font weight of single project name

--- components/WorkSectionChildren.tsx
+++ components/WorkSectionChildren.tsx
@@ -48,7 +48,7 @@ export default function WorkSectionChildren({ workData }: Props) {
                 <p className="mb-2 font-fira text-xs text-secondary-500">
                   Featured Poject
                 </p>
-                <p className="mb-3 font-oswald text-xl font-semibold tracking-wide text-tertiary-500">
+                <p className="mb-3 font-oswald text-xl font-bold text-tertiary-500">
                   {singleFeaturedProject.name}
                 </p>
                 <p className="mb-5">{singleFeaturedProject.description}</p>
