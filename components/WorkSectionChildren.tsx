64f9e9fd9fe960f714a39dde6d1ed8a9abefd122
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-25 18:02:17 +0530

    added margins in mobile view in single project grid

--- components/WorkSectionChildren.tsx
+++ components/WorkSectionChildren.tsx
@@ -20,7 +20,7 @@ export default function WorkSectionChildren({ workData }: Props) {
           return (
             <div
               key={singleFeaturedProject.name}
-              className="grid md:grid-cols-2"
+              className="grid md:grid-cols-2 gap-5 items-center drop-shadow-lg"
             >
               <Splide
                 aria-label="My Favorite Images"
@@ -44,15 +44,15 @@ export default function WorkSectionChildren({ workData }: Props) {
                   );
                 })}
               </Splide>
-              <div>
-                <p className="font-fira text-secondary-500">Featured Poject</p>
-                <p className="font-oswald font-semibold text-tertiary-500">
+              <div className="text-lg">
+                <p className="font-fira text-secondary-500 text-xs mb-2">Featured Poject</p>
+                <p className="font-oswald font-semibold text-tertiary-500 text-xl mb-3 tracking-wide">
                   {singleFeaturedProject.name}
                 </p>
-                <p className="">{singleFeaturedProject.description}</p>
-                <ul className="list-inside list-disc marker:text-secondary-500">
+                <p className="mb-5">{singleFeaturedProject.description}</p>
+                <ul className="list-outside list-disc marker:text-secondary-500 text-tertiary-500 pl-4 mb-5">
                   {singleFeaturedProject.features.map((singleFeature) => {
-                    return <li key={singleFeature}>{singleFeature}</li>;
+                    return <li key={singleFeature} className="">{singleFeature}</li>;
                   })}
                 </ul>
                 <ul className="flex flex-wrap gap-5">
