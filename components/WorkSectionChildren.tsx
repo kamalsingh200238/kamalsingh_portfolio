10e05de4af61d76deeebb3cab096a9cbb9ee3760
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-26 21:42:24 +0530

    changed the font size on the lg breakpoint

--- components/WorkSectionChildren.tsx
+++ components/WorkSectionChildren.tsx
@@ -86,7 +86,7 @@ function FeaturedProjectGrid({
         <p className="mb-2 font-fira text-xs text-secondary-500">
           Featured Poject
         </p>
-        <p className="mb-3 font-oswald text-xl font-semibold text-tertiary-500">
+        <p className="mb-3 font-oswald text-xl font-semibold lg:text-2xl text-tertiary-500">
           {singleFeaturedProject.name}
         </p>
         <p className="mb-5">{singleFeaturedProject.description}</p>
